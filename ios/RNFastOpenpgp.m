745dba8aa0caf45840da08cf70f5f32fd7a76cab
Gerson Alexander Pardo Gamez <jeral@gmail.com>
2019-07-09 12:52:02 -0500

    added ios and framework

--- ios/RNFastOpenpgp.m
+++ ios/RNFastOpenpgp.m
@@ -1,5 +1,6 @@
 
 #import "RNFastOpenpgp.h"
+#import "openpgp/openpgp.h"
 
 @implementation RNFastOpenpgp
 
@@ -9,5 +10,88 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_REMAP_METHOD(encrypt,
+                 encryptWith: (NSString *)message
+                 publicKey: (NSString *)publicKey
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    NSError *error;
+    NSString * output = [OpenpgpNewOpenPGP() encrypt:message publicKey:publicKey error:&error];
+    
+    if(error!=nil){
+        reject([NSString stringWithFormat:@"%ld",[error code]], [error description],error);
+    }else{
+        resolve(output);
+    }
+    
+}
+
+RCT_REMAP_METHOD(decrypt,
+                 decryptWith: (NSString *)message
+                 privateKey: (NSString *)privateKey
+                 passphrase: (NSString *)passphrase
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    NSError *error;
+    NSString * output = [OpenpgpNewOpenPGP() decrypt:message privateKey:privateKey passphrase:passphrase error:&error];
+    
+    if(error!=nil){
+        reject([NSString stringWithFormat:@"%ld",(long)[error code]], [error description],error);
+    }else{
+        resolve(output);
+    }
+    
+}
+
+RCT_REMAP_METHOD(sign,
+                 signWith: (NSString *)message
+                 publicKey: (NSString *)publicKey
+                 privateKey: (NSString *)privateKey
+                 passphrase: (NSString *)passphrase
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    NSError *error;
+    NSString * output = [OpenpgpNewOpenPGP() sign:message publicKey:publicKey privateKey:privateKey passphrase:passphrase error:&error];
+    
+    if(error!=nil){
+        reject([NSString stringWithFormat:@"%ld",(long)[error code]], [error description],error);
+    }else{
+        resolve(output);
+    }
+    
+}
+
+RCT_REMAP_METHOD(verify,
+                 signWith: (NSString *)signature
+                 message: (NSString *)message
+                 publicKey: (NSString *)publicKey
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    NSError *error;
+    bool *result = NULL;
+    BOOL output = [OpenpgpNewOpenPGP() verify:signature message:message publicKey:publicKey ret0_:result error:&error];
+    
+    if(error!=nil){
+        reject([NSString stringWithFormat:@"%ld",[error code]], [error description],error);
+    }else{
+        if(output){
+            resolve(@"1");
+        }else{
+            resolve(@"");
+            
+        }
+    }
+    
+}
+
 @end
   
+
